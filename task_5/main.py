b26b0e06cc6d85ed6dd8de4eb7d3b82256a9f852
Maksim <maksimchetyrbok@gmail.com>
2025-10-24 10:03:44 +0300

    fifth task new

--- task_5/main.py
+++ task_5/main.py
@@ -1,11 +1,5 @@
 mn=int(input("Введите кол-во прошедших минут:"))
-ch=0
-while mn>=60:
-    ch+=1
-  mn-=60
-
-while ch>=24:
-    ch-=24
-
-
-print(ch,"Часов",mn,"Минут")
+ch=mn//60
+ost_mn=mn%60
+ch=ch%24
+print(ch,"Часов",ost_mn,"Минут")
